de09daf236e8bf0f9891b26d9b1f3326f405ead1
maicon777 <m96840422@gmail.com>
2020-12-07 22:45:47 -0300

    Acrescentada traços

--- Projeto teste/Program.cs
+++ Projeto teste/Program.cs
@@ -6,10 +6,12 @@ namespace Projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("........................");
             Console.WriteLine("Hello World!");
             Console.WriteLine("tenha um bom dia");
             Console.WriteLine("estudando programação C#");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("........................");
 
             Console.ReadLine();
         }
