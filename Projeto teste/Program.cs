10f0d772bdc83a0f34a398adddf59fab48160450
maicon777 <m96840422@gmail.com>
2020-12-07 21:47:25 -0300

    Acrescentada a mensagem de despedida

--- Projeto teste/Program.cs
+++ Projeto teste/Program.cs
@@ -9,6 +9,7 @@ namespace Projeto_teste
             Console.WriteLine("Hello World!");
             Console.WriteLine("tenha um bom dia");
             Console.WriteLine("estudando programação C#");
+            Console.WriteLine("Tchau!");
 
             Console.ReadLine();
         }
